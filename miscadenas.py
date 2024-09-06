f3d7d48bbd7a59016c58442c0f0437884ad980c8
Angel-Miguel-Valencia-Carrillo <a.22308051280738@cbtis128.edu.mx>
2024-09-06 09:57:27 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,23 @@
+"""
+    Angel valencia
+    Trabajando con cadenas
+"""
+nombre="Angel"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["A","N","G"]
+for chicas in MisNovias:
+    print(chicas)
+
+    print("otro ejemplo")
+    b = "Angel, Valencia"
+print(b[2:5])
+print(" a mayusculas")
+a = "Angel Valencia"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "perez"
+c = a +" "+ b
+print(c)
